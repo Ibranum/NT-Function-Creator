c67b2873182d957fdf0e4234be80f7fea6a28bd5
Ibranum <evan.read@trojans.dsu.edu>
2022-08-03 13:06:02 -0400

    Removed a million print statements and some commented out code

--- # Nt Function Creator v1.0.py
+++ # Nt Function Creator v1.0.py
@@ -43,7 +43,6 @@ def urlCheck():
             print("[ ❌ ] Invalid URL")
             continue
 
-    #print(urlType)
     return url, urlType
 
 def getMSDNSite(url):
@@ -59,12 +58,9 @@ def getMSDNSite(url):
         #driver.switch_to.frame("content")
         html = driver.page_source
         time.sleep(2)
-        #print(html)
 
         xpathOutput = driver.find_element(By.XPATH, targetXPath).text
         functionName = driver.find_element(By.XPATH, "/html/body/div[2]/div/section/div/div[1]/main/div[3]/pre/code/span/span/span[1]").text
-        #print(xpathOutput)
-        #print(functionName)
 
         driver.close()
 
@@ -92,12 +88,9 @@ def getNTInternalSite(url):
         driver.switch_to.frame("content")
         html = driver.page_source
         time.sleep(2)
-        #print(html)
 
         xpathOutput = driver.find_element(By.XPATH, targetXPath).text
         functionName = driver.find_element(By.XPATH, "/html/body/div[2]").text
-        #print(xpathOutput)
-        #print(functionName)
 
         driver.close()
 
@@ -108,18 +101,11 @@ def getNTInternalSite(url):
         exit()
 
 def ptypeRegex(xpathOutput, urlType, functionName):
-    #print(xpathOutput)
-    
     if urlType == "ntinternals":
         inOrOut = []
         typeField = re.findall("(?:OUT) \w+|(?:IN )\w+", xpathOutput)
-        #print(typeField)
         for i in range(len(typeField)):
             inOrOut.append(typeField[i].split(" "))
-            #print(typeField[i])
-            #print(i)
-
-        #print(inOrOut)
 
         return inOrOut
 
@@ -127,16 +113,10 @@ def ptypeRegex(xpathOutput, urlType, functionName):
         inOrOut = []
         xClean = 1
         xpathOutput = cleanMSDN(xpathOutput, functionName, xClean)
-        #print("xpath:" + xpathOutput)
         
         typeField = re.findall("[A-Z_]{2,20}", xpathOutput)
-        #print(typeField)
         for i in range(len(typeField)):
             inOrOut.append(typeField[i].split(" "))
-            #print(typeField[i])
-            #print(i)
-
-        #print("inorout: " + (str(inOrOut)))
 
         return inOrOut
 
@@ -165,32 +145,15 @@ def printPtypes(inOrOut, urlType):
 
 def pnamesRegex(xpathOutput, inOrOut, urlType, functionName):
     if urlType == "ntinternals":
-    
-        # Need to grab pNames using Regex now
-        # (\D \w+)
-        #print(xpathOutput)
-        # cut out optionals? then use regex
-        #print(xpathOutput)
-        #print("\n")
 
         for i in range(len(inOrOut)):
             if inOrOut[i][1] in xpathOutput:
-                #print(inOrOut[i][1])
                 xpathOutput = xpathOutput.replace(inOrOut[i][1], "")
-                #print(xpathOutput)
-                #print("\n")
 
         xpathOutput = str(xpathOutput)
         removalChars = [" OPTIONAL", "IN", "OUT", "NTAPI", "NTSTATUS", "NTSYSAPI"]
         for i in range(len(removalChars)):
             xpathOutput = xpathOutput.replace(removalChars[i], "")
-        #xpathOutput = xpathOutput.replace(" OPTIONAL", '')
-        #xpathOutput = xpathOutput.replace("IN", "")
-        #xpathOutput = xpathOutput.replace("OUT", "")
-        #xpathOutput = xpathOutput.replace("NTAPI", "")
-        #xpathOutput = xpathOutput.replace("NTSTATUS", "")
-        #xpathOutput = xpathOutput.replace("NTSYSAPI", "")
-        #xpathOutput = xpathOutput.replace(" ", '')
         xpathOutput = xpathOutput.replace(")", '    ')
         xpathOutput = xpathOutput.replace(",", '    ')
 
@@ -198,7 +161,6 @@ def pnamesRegex(xpathOutput, inOrOut, urlType, functionName):
 
         listNames = []
         typeField = re.findall("(\w+ )", xpathOutput)
-        #typeField = typeField.replace(" ", '')
 
         for i in range(len(typeField)):
             typeField[i] = typeField[i].replace(" ", '')
@@ -206,51 +168,29 @@ def pnamesRegex(xpathOutput, inOrOut, urlType, functionName):
         return typeField
     
     elif urlType == "msdn":
-        #print(xpathOutput)
-        #print("\n")
-        #print(inOrOut)
-
-        #print(xpathOutput)
         xClean = 1
         xpathOutput = cleanMSDN(xpathOutput, functionName, xClean)
-        #print(xpathOutput) # <--- problem area
-        #print(inOrOut)
         xClean = 0
         inOrOut = cleanMSDN(str(inOrOut), functionName, xClean)
 
         inOrOut = inOrOut.split()
-        #print(inOrOut)
-        #print(xpathOutput)
 
         for i in range(len(inOrOut)):
             if inOrOut[i] in xpathOutput:
-                #print(inOrOut[i])
                 xpathOutput = xpathOutput.replace(inOrOut[i], "")
-                #print(xpathOutput)
-                #print("reached")
-                #print("\n")
-
-        #xpathOutput = str(xpathOutput)
-        #print(xpathOutput)
 
         # This is where it used to iterate through to make sure no pTypes were present
 
         listNames = []
         typeField = re.findall("\w+", xpathOutput)
-        #typeField = typeField.replace(" ", '')
-        #print(xpathOutput)
 
         for i in range(len(typeField)):
             typeField[i] = typeField[i].replace(" ", '')
 
-        #print(typeField) #<--- problem here
-        
         for i in range(len(typeField)-1): # why does this work???????
             #print(typeField[i])
             if "Zw" in str(typeField[i]):
                 typeField.pop(i)
-
-        #print(typeField)
 
         return typeField
 
@@ -296,8 +236,7 @@ def cleanMSDN(xpathOutput, functionName, xClean):
 
     if xClean == 1:
         xpathOutput = xpathOutput.replace("NTSTATUS", '', 1)
-    
-    #print(xpathOutput)
+
     return xpathOutput
 
 def main():
@@ -307,21 +246,17 @@ def main():
     functionName = ""
 
     url, urlType = urlCheck() # Get and check URL from user
-    #print(urlType)
+
     if urlType == "ntinternals":
         xpathOutput, functionName = getNTInternalSite(url) # Use Selenium to grab info from user URL
-        #print(xpathOutput)
         inOrOut = ptypeRegex(xpathOutput, urlType, functionName) # Get ptypes from the xpath output using Regex
         pnames = pnamesRegex(xpathOutput, inOrOut, urlType, functionName) # Get pnames from the xpath output using Regex
     elif urlType == "msdn":
         xpathOutput, functionName = getMSDNSite(url) # Use Selenium to grab info from user URL
-        #print(xpathOutput)
-        #print(functionName)
+
         inOrOut = ptypeRegex(xpathOutput, urlType, functionName) # Get ptypes from the xpath output using Regex
         pnames = pnamesRegex(xpathOutput, inOrOut, urlType, functionName) # Get pnames from the xpath output using Regex
 
-    #printPtypes(inOrOut) # Print pTypes
-    #printPnames(pnames) # Print pNames
     if urlType == "msdn":
         xClean = 0
         inOrOut = cleanMSDN(inOrOut, functionName, xClean)
